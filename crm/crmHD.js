97c80d852614bd75cef9f6e8ed70115568cfb5fd
Relax <201888149@qq.com>
2017-01-25 23:44:01 +0800

    AA1

--- crm/crmHD.js
+++ crm/crmHD.js
@@ -11,12 +11,12 @@ var soap = require('soap'),
     crypto = require('crypto'),
     verify = require('../Tools/verify');
 
-var wsdlPath = 'http://mall.demo.hd123.cn:8080/HDCRMWebService.dll/wsdl/IHDCRMWebService',
-    sOper = 'HDCRM[0]',
-    sStoreCode = '9517',
+var wsdlPath = 'http://112.74.47.20:84/HDCRMWebService.dll/wsdl/IHDCRMWebService',
+    sOper = '000[WEB员工]',
+    sStoreCode = '0202',
     sWorkStation = 'ZGC-20151203QYD',
-    nUserGid = '1000060',
-    sUserPwd = '33D51D50BCDD575B';
+    nUserGid = '1000300',
+    sUserPwd = 'D81EF475940D2DD4';
 var defaultGrade = '0001';  //默认开卡等级
 
 //登录  得到Cookie
