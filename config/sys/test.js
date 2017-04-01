9dd187ee5bc7154ba825d6bb8c182bae3fcb405c
Relax <wx_relax@outlook.com>
2017-04-01 14:54:34 +0800

    AAAA

--- config/sys/test.js
+++ config/sys/test.js
@@ -17,7 +17,7 @@ module.exports = {
     //     },
     // }
     //mongodb: 'mongodb://120.25.122.48:20164/member_conduct', 
-    mongodb : 'mongodb://127.0.0.1:27017/ketuo',
+    mongodb : 'mongodb://120.25.122.48:20164/member_conduct_test',
     cardBinding: 'cardBinding',
     parkingPayRecord: 'parkingPayRecord',
     prepayCard: 'prepayCards',
