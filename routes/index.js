002c4617fb16696327d876b411e1dda0c4240012
Relax <wx_relax@outlook.com>
2017-04-25 11:02:18 +0800

    AA

--- routes/index.js
+++ routes/index.js
@@ -236,6 +236,7 @@ router.post('/JSCardDetialFee', function (req, res, next) {
         },
         function (token, carDetial, cb) {
             jieshun.PlaceOrder(jieshun_config, token, carNo, function (err, result) {
+                console.log('PlaceOrder err', err, 'result', result);
                 if (err) {
                     cb(err);
                 } else {
